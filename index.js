53692d336d925a991fefb57ff85d86e2134f937d
KyleAres <kylealtimore@gmail.com>
2018-11-06 15:25:59 -0600

    gay

--- index.js
+++ index.js
@@ -2,15 +2,41 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const { token, prefix } = require("./config.json");
 
-client.on("ready", () {
- console.log("I am ready!");
+client.on("ready", () => {
+ // console.log("I am ready!");
+ // This event will run if the bot starts, and logs in, successfully.
+ console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+ // Example of changing the bot's playing game to something useful. `client.user` is what the
+ // docs refer to as the "ClientUser".
+ client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
-client.on("message", (message)  {
- if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+client.on("message", async message => {
+ // This event will run on every single message received, from any channel or DM.
+ 
+ // It's good practice to ignore other bots. This also makes your bot ignore itself
+ // and not get into a spam loop (we call that "botception").
+ if(message.author.bot) return;
+ 
+ // Also good practice to ignore any message that does not start with our prefix, 
+ // which is set in the configuration file.
+ if(message.content.indexOf(prefix) !== 0) return;
+ 
+ // Here we separate our "command" name, and our "arguments" for the command. 
+ // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+ // command = say
+ // args = ["Is", "this", "the", "real", "life?"]
+ const args = message.content.slice(prefix.length).trim().split(/ +/g);
+ const command = args.shift().toLowerCase();
+ 
+ /*if (!message.content.startsWith(prefix) || message.author.bot) return;
  if (message.content.startsWith(prefix + "ping")) {
-  message.channel.send("pong!");
-} else
+  message.channel.send("pong!");*/
+}
+
+else
+
 if (message.content.startsWith(prefix + "foo")) {
  message.channel.send("bar!");
  }
