7cb0625f41f39ec946f15c08bc31eda2cbd5e994
KyleAres <kylealtimore@gmail.com>
2018-11-07 03:45:58 -0600

    yes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { token, prefix } = require("./config.json");
 client.on("ready", () => console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`));
  // Example of changing the bot's playing game to something useful. `client.user` is what the
  // docs refer to as the "ClientUser".
-client.user.setActivity(`Serving ${client.guilds.size} servers`);
+//client.user.setActivity(`Serving ${client.guilds.size} servers`);
 
 
 
